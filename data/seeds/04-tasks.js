c439bedda7eafd764c749767e4f95ef262a6d33d
lilyhoratio <lilyhzhou@gmail.com>
2019-09-13 09:43:44 -0700

    seeded all tables

---
+++ data/seeds/04-tasks.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex) {
+  return knex("tasks")
+    .truncate()
+    .then(function() {
+      return knex("tasks").insert([
+        {
+          description: "Search for software engineers",
+          notes: "Limit to SF bay area",
+          completed: true,
+          project_id: 1
+        },
+        { description: "Print out resume", completed: false, project_id: 1 },
+        { description: "Docker tutorial", completed: false, project_id: 2 },
+        {
+          description: "Schedule time with Henry",
+          completed: false,
+          project_id: 2
+        },
+        {
+          description: "Figure out how to link domain to code",
+          completed: false,
+          project_id: 3
+        },
+        {
+          description: "Ask for feedback from friends",
+          completed: false,
+          project_id: 3
+        }
+      ]);
+    });
+};
